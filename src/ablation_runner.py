2435aadbfa99041605fabe97c4b3f12b130d42a0
vafaeim <vafaeim@icloud.com>
2026-09-19 01:28:13 +0330

    perf: strictly limit numpy threading to prevent context switching thrash with PyTorch multiprocessing

--- src/ablation_runner.py
+++ src/ablation_runner.py
@@ -1,3 +1,10 @@
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+
 """
 Systematic Component-Wise Ablation Studies Runner for SE-ResNet Acoustic Speed Estimation.
 
--- src/train_engine.py
+++ src/train_engine.py
@@ -1,4 +1,11 @@
 import os
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+
+import os
 import copy
 from typing import Dict, Any
 import numpy as np
